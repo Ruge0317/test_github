c4c0da66f3b4322292717de5ce12b18965316f93
Ruge <eiwyoo@gmail.com>
2025-02-18 17:06:30 +0800

    0218 is finish

---
+++ 0217bd4_sample.py
@@ -0,0 +1,48 @@
+"""
+pip install virtualenv
+
+啟動虛擬環境: .\scraper_env\Scripts\activate   
+"""
+
+
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html>
+ <head>
+  <title>
+   The Dormouse's story
+  </title>
+ </head>
+ <body>
+  <p class="title">
+   <b>
+    The Dormouse's story
+   </b>
+  </p>
+  <p class="story">
+   Once upon a time there were three little sisters; and their names were
+   <a class="sister" href="http://example.com/elsie" id="link1">
+    Elsie
+   </a>
+   ,
+   <a class="sister" href="http://example.com/lacie" id="link2">
+    Lacie
+   </a>
+   and
+   <a class="sister" href="http://example.com/tillie" id="link2">
+    Tillie
+   </a>
+   ; and they lived at the bottom of a well.
+  </p>
+  <p class="story">
+   ...
+  </p>
+ </body>
+</html>
+
+
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')       #parse = 解析
+print(soup.prettify())
